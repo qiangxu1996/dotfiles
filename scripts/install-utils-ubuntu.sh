bedebc57b10e891f823f1237bf33aaca03050344
Qiang Xu <qiangxu1996@gmail.com>
2023-06-04 21:06:35 -0400

    support ubuntu packages

---
+++ scripts/install-utils-ubuntu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# https://github.com/BurntSushi/ripgrep#installation
+apt-get install exa fd-find fzf lua5.4 ripgrep zsh
+snap install --classic node
+npm install -g tldr
+snap install --classic nvim
